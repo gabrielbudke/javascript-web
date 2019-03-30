3ea1348d42beb1c16be13fd7812452887b61e620
Gabriel Budke <gabrielbudke@gmail.com>
2019-03-30 13:13:28 -0300

    Material sobre array

--- array/foreach1.js
+++ array/foreach1.js
@@ -3,8 +3,8 @@ const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 // A função callback do 'forEach' suportar 3 argumentos
 // nome, indice e o proprio array
 aprovados.forEach(function(nome, indice, array) {
-    console.log(`${indice + 1}) ${nome}`)
-    console.log(array)
+    console.log(`${indice + 1}) ${nome}`);
+    console.log(array);
 })
 
 aprovados.forEach(nome => console.log(nome))

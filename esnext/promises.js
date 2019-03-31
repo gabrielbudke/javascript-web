74afd18fbe421d85f4a958b624bdde2b568b5937
Gabriel Budke <gabrielbudke@gmail.com>
2019-03-30 22:10:04 -0300

    Promise

---
+++ esnext/promises.js
@@ -0,0 +1,25 @@
+/**
+ * Promises
+ * 
+ *  > Tradução de promessa
+ *  > Usando quando se quer algum tipo de processamento
+ *      assíncrono
+ *  > Tem dois grandes destinos:
+ *      Ser resolvida ou rejeitada
+ * 
+ *  > Promise aceita somente um parâmetro
+ * 
+ */
+
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(frase)
+        }, segundos * 1000)
+    })
+}
+
+falarDepoisDe(3, 'Que legal!')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase))
+    .catch(e => console.log(e))

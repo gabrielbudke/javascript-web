7d09273fb30bd297a0ac562651057bae210882ae
Gabriel Budke <gabrielbudke@gmail.com>
2019-04-01 23:57:50 -0300

    ESNext

---
+++ esnext/asyncAwait.js
@@ -0,0 +1,38 @@
+// sem promise...
+const http = require('http');
+
+const getTurma = letra => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`;
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = '';
+    
+            res.on('data', dados => {
+                resultado += dados;
+            });
+    
+            res.on('end', () => {
+                try{
+                    resolve(JSON.parse(resultado));
+                } catch(erro){
+                    reject(erro);
+                }
+            });
+        });
+    });
+}
+
+// Recurso do ES8
+// Objetivo de simplificar o uso de promises...
+// > Primeiro: é preciso criar uma função e 
+//   marcar ela com 'async'
+let obterAlunos = async () => {
+    const turmaA = await getTurma('A');
+    const turmaB = await getTurma('B');
+    const turmaC = await getTurma('C');
+    return [].concat(turmaA, turmaB, turmaC);
+}
+
+obterAlunos()
+    .then(alunos => alunos.map(a => a.nome))
+    .then(nomes => console.log(nomes))
